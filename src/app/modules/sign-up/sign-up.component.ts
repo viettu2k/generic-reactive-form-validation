9a373817672fb1ca415b8073b2360d2edfd4cc8a
viettu2k <doanviettu2k@gmail.com>
2022-07-22 09:33:16 +0700

    Sign up form component

--- src/app/modules/sign-up/sign-up.component.ts
+++ src/app/modules/sign-up/sign-up.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+import { PasswordMatcher } from "../../shared/password-matcher";
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  selector: "app-sign-up",
+  templateUrl: "./sign-up.component.html",
+  styleUrls: ["./sign-up.component.scss"],
 })
 export class SignUpComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor() { }
+  // Use with the generic validation message class
+  displayMessage: { [key: string]: string } = {};
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.signupForm = this.fb.group(
+      {
+        firstName: ["", [Validators.required]],
+        lastName: ["", [Validators.required]],
+        email: ["", [Validators.required], Validators.email],
+        password: ["", [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ["", [Validators.required]],
+      },
+      { validator: PasswordMatcher.match }
+    );
   }
 
+  signup() {
+    console.log("---form", this.signupForm.value);
+  }
 }
