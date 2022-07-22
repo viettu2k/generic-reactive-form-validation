723c2a469638661f228a7f48e447559d87fbcd90
viettu2k <doanviettu2k@gmail.com>
2022-07-22 10:00:08 +0700

    Generic validation on Sign up

--- src/app/modules/sign-up/sign-up.component.ts
+++ src/app/modules/sign-up/sign-up.component.ts
@@ -1,6 +1,20 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-
+import {
+  Component,
+  OnInit,
+  ViewChildren,
+  ElementRef,
+  AfterViewInit,
+} from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControlName,
+  AbstractControl,
+} from "@angular/forms";
+import { Observable, fromEvent, merge } from "rxjs";
+import { debounceTime } from "rxjs/operators";
+import { GenericValidator } from "../../shared/generic-validator";
 import { PasswordMatcher } from "../../shared/password-matcher";
 
 @Component({
@@ -9,12 +23,19 @@ import { PasswordMatcher } from "../../shared/password-matcher";
   styleUrls: ["./sign-up.component.scss"],
 })
 export class SignUpComponent implements OnInit {
+  // Access every form input fields in our signup html file
+  @ViewChildren(FormControlName, { read: ElementRef })
+  formInputElements: ElementRef[];
   signupForm: FormGroup;
 
   // Use with the generic validation message class
   displayMessage: { [key: string]: string } = {};
+  private genericValidator: GenericValidator;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    // Define an instance of the validator for use with this form,
+    this.genericValidator = new GenericValidator();
+  }
 
   ngOnInit() {
     this.signupForm = this.fb.group(
@@ -27,6 +48,22 @@ export class SignUpComponent implements OnInit {
       },
       { validator: PasswordMatcher.match }
     );
+  }
+
+  ngAfterViewInit(): void {
+    // Watch for the blur event from any input element on the form.
+    const controlBlurs: Observable<any>[] = this.formInputElements.map(
+      (formControl: ElementRef) => fromEvent(formControl.nativeElement, "blur")
+    );
+
+    // Merge the blur event observable with the valueChanges observable
+    merge(this.signupForm.valueChanges, ...controlBlurs)
+      .pipe(debounceTime(800))
+      .subscribe((value) => {
+        this.displayMessage = this.genericValidator.processMessages(
+          this.signupForm
+        );
+      });
   }
 
   signup() {
